ffdeaebe8acd2de6121c96e63f9d980664348823
murilopapa <murilopapa123@gmail.com>
2019-04-11 09:00:12 -0300

    contadora de fotos

--- video_recognition_faster.py
+++ video_recognition_faster.py
@@ -1,46 +1,54 @@
 import face_recognition
 import cv2
+import os
 
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
 #   2. Only detect faces in every other frame of video.
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
-
-# Get a reference to webcam #0 (the default one)
-
 escale = 0.25
-camera_default = 0      #0 é a webcam nativa, ver o print do loop abaixo para ver as webcans disponiveis
-
+# 0 é a webcam nativa, ver o print do loop abaixo para ver as webcans disponiveis
+camera_default = 0
+"""
+#loop para testar 10 cameras e ver quais estao disponiveis
 cams_test = 10
 for i in range(0, cams_test):
     cap = cv2.VideoCapture(i)
     test, frame = cap.read()
     print("i : "+str(i)+" /// result: "+str(test))
-
+"""
 video_capture = cv2.VideoCapture(camera_default)
 
+directory = os.fsencode("imagens/faces")
+count = 0
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        count = count + 1
+        continue
+    else:
+        continue
+print("Banco de dados: {} imagens".format(count))
+
 # Load a sample picture and learn how to recognize it.
-fer_image = face_recognition.load_image_file("imagens/fer.jpg")
+fer_image = face_recognition.load_image_file("imagens/faces/fer.jpg")
 fer_face_encoding = face_recognition.face_encodings(fer_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-fallen_image = face_recognition.load_image_file("imagens/fallen.jpg")
+fallen_image = face_recognition.load_image_file("imagens/faces/fallen.jpg")
 fallen_face_encoding = face_recognition.face_encodings(fallen_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-murilo_image = face_recognition.load_image_file("imagens/murilo.jpg")
+murilo_image = face_recognition.load_image_file("imagens/faces/murilo.jpg")
 murilo_face_encoding = face_recognition.face_encodings(murilo_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-pupo_image = face_recognition.load_image_file("imagens/pupo.jpg")
+pupo_image = face_recognition.load_image_file("imagens/faces/pupo.jpg")
 pupo_face_encoding = face_recognition.face_encodings(pupo_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-ettore_image = face_recognition.load_image_file("imagens/ettore.jpg")
+ettore_image = face_recognition.load_image_file("imagens/faces/ettore.jpg")
 ettore_face_encoding = face_recognition.face_encodings(ettore_image)[0]
 
 # Create arrays of known face encodings and their names
@@ -79,41 +87,46 @@ while True:
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_encodings = face_recognition.face_encodings(
+            rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            matches = face_recognition.compare_faces(
+                known_face_encodings, face_encoding)
             name = "Unknown"
-            color = (0, 255, 0)
 
             # If a match was found in known_face_encodings, just use the first one.
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
-                color = (0, 0, 255)
 
             face_names.append(name)
 
     process_this_frame = not process_this_frame
 
-
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= escale*16
-        right *= escale*16
-        bottom *= escale*16
-        left *= escale*16
+        top *= int(escale*16)
+        right *= int(escale*16)
+        bottom *= int(escale*16)
+        left *= int(escale*16)
+
+        color = (0, 0, 255)
+        if "Unknown" in name:
+            color = (0, 255, 0)
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
 
         # Draw a label with a name below the face
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), color, cv2.FILLED)
+        cv2.rectangle(frame, (left, bottom - 35),
+                      (right, bottom), color, cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6),
+                    font, 1.0, (255, 255, 255), 1)
 
     # Display the resulting image
     cv2.imshow('Video', frame)
