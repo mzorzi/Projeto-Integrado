72c3f27c738f73c2697d08b7e8b881c1597c3ba5
Victor-H-Nascimento <nascimento.victor01@gmail.com>
2019-06-09 15:15:30 -0300

    Running with 8 threads

--- Face Recognition GITHUB/video_recognition_file.py
+++ Face Recognition GITHUB/video_recognition_file.py
@@ -18,7 +18,7 @@ def func(frame_recived, known_face_encodings_recived, frame_number_recived, outp
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
     elapsed_time = time.time() - start_time
-    #print("[INFO][{}] Tempo para achar rostos: {}".format(frame_number_recived,elapsed_time))
+    print("[INFO][{}] Tempo para achar rostos: {}".format(frame_number_recived,elapsed_time))
     face_names = []
     start_time = time.time()
     for face_encoding in face_encodings:
@@ -35,7 +35,7 @@ def func(frame_recived, known_face_encodings_recived, frame_number_recived, outp
         face_names.append(name)
 
     elapsed_time = time.time() - start_time
-    #print("[INFO][{}] Tempo para localizar faces: {}".format(frame_number_recived,elapsed_time))
+    print("[INFO][{}] Tempo para localizar faces: {}".format(frame_number_recived,elapsed_time))
 
     # Label the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -61,9 +61,9 @@ def func(frame_recived, known_face_encodings_recived, frame_number_recived, outp
         if frame_number_recived == rec:
             queue_images_recived.put(frame_recived)
             #output_movie_recived.write(frame_recived)   
-            queue_recived.put(rec+1)
-            if (frame_number_recived % 100) == 0:
-                print("[INFO][{}] Writing frame {} / {}".format(frame_number_recived, frame_number_recived, length))
+            queue_recived.put(rec+2)
+            #if (frame_number_recived % 100) == 0:
+            print("[INFO][{}] Writing frame {} / {}".format(frame_number_recived, frame_number_recived, length))
             
             break
 
@@ -82,12 +82,12 @@ if __name__ == '__main__':
     # specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
     # Open the input movie file
-    input_movie = cv2.VideoCapture("/home/murilo/Github/Dataset P.I/praca4k20sec.mp4")
+    input_movie = cv2.VideoCapture("/home/victor/Downloads/praca4k20sec.mp4")
     length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
 
     # Create an output movie file (make sure resolution/frame rate matches input video!)
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    output_movie = cv2.VideoWriter('output.avi', fourcc, 30, (3840, 2160))
+    output_movie = cv2.VideoWriter('output.avi', fourcc, 30, (1080, 720))
 
     path = "imagens/"
 
@@ -122,7 +122,8 @@ if __name__ == '__main__':
     queue.put(1)
     while process_this_frame:
         jobs = []
-        for i in range(0, 4):
+        counter = 0
+        for i in range(0, 16):
         # Grab a single frame of video
             ret, frame = input_movie.read()
             frame_number += 1
@@ -133,17 +134,21 @@ if __name__ == '__main__':
             if not ret:
                 process_this_frame = False
                 break
-            process = multiprocessing.Process(target=func, args=(frame, known_face_encodings, frame_number, output_movie, queue, queue_images))
-            jobs.append(process)
-            process.start()
-            #print("Processo da frame {} iniciado".format(frame_number))
-
-        
-
-        for i in range(0,4):
+            if i % 2 == 0:
+                process = multiprocessing.Process(target=func, args=(frame, known_face_encodings, frame_number, output_movie, queue, queue_images))
+                jobs.append(process)
+                process.start()
+                print("Processo da frame {} iniciado".format(frame_number))
+            else:
+                queue_images.put(frame)
+                print("Nao Processando a frame {}".format(frame_number))
+            counter += 1
+                
+        for i in range(0,counter):
             frame_write = queue_images.get()
+            frame_write = cv2.resize(frame_write,(1080,720))
             output_movie.write(frame_write) 
-           
+        
         #print("Aguardando fim dos processos")
         for j in jobs:
             j.join()
