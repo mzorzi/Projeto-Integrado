a3019e9f5642bb7ed5457641d1b797f25b1eec26
murilopapa <murilopapa123@gmail.com>
2019-07-11 18:28:28 -0300

    final?

--- Face Recognition GITHUB/video_recognition_gpu.py
+++ Face Recognition GITHUB/video_recognition_gpu.py
@@ -18,16 +18,22 @@ import os
 # Open video file
 start_total_time = time.time()
 
-video_capture = cv2.VideoCapture("/home/murilo/Github/Dataset PI/praca4k20sec.mp4")
+video_capture = cv2.VideoCapture("/home/murilo/Github/Dataset PI/praca4k5min.mp4")
 length = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
 batch_size = 2
 frames = []
 frame_count = 0
-
+frames_to_process = 10   #significa 1 a cada X frames
 output_name = "GPUoutput1.avi"
 path = "imagens/"
 acuracia_minima = 0.5
+resolucao_output = (3840, 2160)
+
+# 4k 3840 x 2160
+# fullHD 1080x720
+
+
 # Load some sample pictures and learn how to recognize them.
 directory = os.fsencode(path)
 count = 0
@@ -52,7 +58,7 @@ frame_number = 0
 
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-output_movie = cv2.VideoWriter(output_name, fourcc, 30, (1280, 720))
+output_movie = cv2.VideoWriter(output_name, fourcc, 30, resolucao_output)
 
 
 
@@ -73,23 +79,38 @@ while video_capture.isOpened():
     # Save each frame of the video to a list
     frame_count += 1
     frames.append(frame)
-    # Every 128 frames (the default batch size), batch process the list of frames to find faces
-    if len(frames) == batch_size:
+
+    if len(frames) == batch_size*frames_to_process:
+        frames_to_batch = []
+        for j in range(0, batch_size*frames_to_process):
+            
+            if j%frames_to_process == 0:
+                frames_to_batch.append(frames[j])
+
         batch_time = time.time()
-        batch_of_face_locations = face_recognition.batch_face_locations(frames, number_of_times_to_upsample=0, batch_size=batch_size)
+
+        batch_of_face_locations = face_recognition.batch_face_locations(frames_to_batch, number_of_times_to_upsample=0, batch_size=batch_size)
+
         final_batch_time = time.time() - batch_time
 
-        tempo_restante = (length-frame_count)/batch_size * final_batch_time
-        tempo_restante_h = tempo_restante / 3600
-        tempo_restante_m = (tempo_restante / 60) % 60
-        tempo_restante_s = tempo_restante % 60
-        print("Tempo restante: {:.0f}:{:.0f}:{:.0f}".format(tempo_restante_h, tempo_restante_m, tempo_restante_s), end='\r')
-        #print("Tempo do batch_face_locations para {} frames: {}".format(batch_size, final_batch_time))
+
+        tempo_restante = (length-frame_count)/(batch_size*frames_to_process) * final_batch_time
+
+        tempo_restante_h = int(tempo_restante/3600)
+
+        tempo_restante_m = int ((tempo_restante - (tempo_restante_h*3600)) /60)
+
+        tempo_restante_s = int((tempo_restante -((tempo_restante_h*3600) + (tempo_restante_m *60))))
+
+        os.system('clear')
+        print("Tempo do batch_face_locations para {} a cada {} frames: {}".format(batch_size, frames_to_process, final_batch_time))
+        print("Frames restantes: {}".format(length-frame_count))
+        print("Tempo restante: {}:{}:{}".format(tempo_restante_h, tempo_restante_m, tempo_restante_s))
         
 
         aux = 0
         for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
-            face_encodings = face_recognition.face_encodings(frames[aux], face_locations)
+            face_encodings = face_recognition.face_encodings(frames_to_batch[aux], face_locations)
             face_names = []
             for face_encoding in face_encodings:
                 # See if the face is a match for the known face(s)
@@ -119,21 +140,44 @@ while video_capture.isOpened():
                 if not name:
                     continue
                 color = (0,0,255)
-                if "Unknown" in name:
+                if unknown_face in name:
                     color = (0,255,0)
                 else:
                     face_names.append(name)
 
                     # Draw a box around the face
-                cv2.rectangle(frames[aux], (left, top), (right, bottom), color, 2)
+                cv2.rectangle(frames_to_batch[aux], (left, top), (right, bottom), color, 2)
 
                     # Draw a label with a name below the face
-                cv2.rectangle(frames[aux], (left, bottom - 25), (right, bottom), color, 2)
-                cv2.putText(frames[aux], name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
-            frame_to_write = frames[aux]
-            frame_to_write = cv2.resize(frames[aux],(1280,720))
+                cv2.rectangle(frames_to_batch[aux], (left, bottom - 25), (right, bottom), color, 2)
+                cv2.putText(frames_to_batch[aux], name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
+
+            frame_to_write = frames_to_batch[aux]
+            frame_to_write = cv2.resize(frame_to_write,resolucao_output)
             output_movie.write(frame_to_write)
-            i+=1
+
+            for j in range(aux*frames_to_process, aux*frames_to_process + frames_to_process):
+                if j!=aux*frames_to_process:
+                    for (top, right, bottom, left), name in zip(face_locations, face_names):
+                        if not name:
+                            continue
+                        color = (0,0,255)
+                        if unknown_face in name:
+                            color = (0,255,0)
+                        else:
+                            face_names.append(name)
+
+                            # Draw a box around the face
+                        cv2.rectangle(frames[j], (left, top), (right, bottom), color, 2)
+
+                            # Draw a label with a name below the face
+                        cv2.rectangle(frames[j], (left, bottom - 25), (right, bottom), color, 2)
+                        cv2.putText(frames[j], name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 255, 255), 2)
+                    frame_to_write = frames[j]
+                    frame_to_write = cv2.resize(frame_to_write,resolucao_output)
+                    output_movie.write(frame_to_write)
+
+            aux+=1
 
         # Clear the frames array to start the next batch
         frames = []
